206393faceb4627518bde7fcf76f6285301079d6
Bruce Guenter <Bruce@FutureQuest.net>
2014-06-26 14:10:46 -0600

    python: Documentation updates and completion of some handling

--- python/fqapi/__init__.py
+++ python/fqapi/__init__.py
@@ -1,12 +1,12 @@
 '''
-This module provides the low-level network connections to the FQ API
+This module provides the low-level network connections to the FQ CNC API
 daemon, either through a local UNIX-domain socket or through HTTPS.
 
-Both of the client classes implement a single function, "request".  Pass
-it the method (ie "GET"), resource path (ie "/1/server"), and optionally
-a Python dict containing the input data for POST and PUT requests.  It
-returns a Python dict containing the response data or raises the
-exception fqapi.Error.
+The client object implmenents functions for doing low-level access to
+the server: get, put, post, and delete.  Pass these the resource path
+(ie "/1/server"), and optionally a Python dict containing the input data
+for POST and PUT requests.  They return a Python dict containing the
+response data or raise the exception fqapi.Error.
 
 For example:
 
@@ -29,12 +29,19 @@ import fqapi.ftp
 _local_socket_path = '/var/run/FQapi'
 _remote_port = 987
 
-def format_request(method, path, body, headers):
+import socket, ssl
+
+class HTTPSConnection(httplib.HTTPSConnection):
+        def connect(self):
+                sock = socket.create_connection((self.host, self.port),
+                                                self.timeout, self.source_address)
+                self.sock = ssl.wrap_socket(sock, self.key_file, self.cert_file, ssl_version=ssl.PROTOCOL_TLSv1)
+
+def _format_request(method, path, body, headers):
 	request = '%s %s HTTP/1.0\r\n' % (method, path)
 	for h in headers:
 		request += '%s: %s\r\n' (h, headers[h])
 	if body is not None:
-		body = json.dumps(body)
 		request += 'Content-Length: %d\r\n' % len(body)
 		request += 'Content-Type: application/json\r\n'
 	request += '\r\n'
@@ -42,12 +49,19 @@ def format_request(method, path, body, headers):
 		request += body
 	return request
 
-def read_response(resp):
+def _read_response(resp):
 	body = resp.read()
 	body = json.loads(body) if body else None
 	return resp.status, resp.reason, body
 
 class Error(Exception):
+	'''
+	Error class used by all low-level client code.
+
+	Object data:
+	code -- the numerical code returned by the server, ie 500
+	msg -- the text of the error message from the server
+	'''
 	def __init__(self, code, msg):
 		self.code = code
 		self.msg = msg
@@ -57,47 +71,79 @@ class Error(Exception):
 		return "fqapi.Error(%r, %r)" % (self.code, self.msg)
 
 class GenericClient:
-	def request(self, path, body=None):
-		code,msg,data = self._request(path, body)
-		if code <> 200:
+	'''
+	Generic FQ CNC API client class.
+	'''
+	def request(self, method, path, body=None):
+		"""Make a request to the server.
+
+		This routine is responsible for handling generic requests to the
+		server.  Returns a dict or None, or raises fqapi.Error if the
+		request failed.  Typically this routine would not be called by
+		client code.  Use the get, put, post, and delete wrappers
+		instead.
+
+		Parameters:
+		method -- the HTTP method to invoke
+		path -- the absolute path to the resource
+		body -- the data given to POST and PUT requests
+		"""
+		body = json.dumps(body) if body is not None else None
+		code,msg,data = self._request(method, path, body)
+		if code < 200 or code >= 300:
 			raise Error(code, msg)
-		return data
+		return code,msg,data
 	def get(self, path):
+		"""Issue a GET request."""
 		return self.request('GET', path)
 	def put(self, path, body):
+		"""Issue a PUT request."""
 		return self.request('PUT', path, body)
 	def post(self, path, body):
+		"""Issue a POST request."""
 		return self.request('POST', path, body)
 	def delete(self, path):
+		"""Issue a DELETE request."""
 		return self.request('DELETE', path)
 
 	def ftp(self):
+		"""Return a new fqapi.ftp.FTP object."""
 		return fqapi.ftp.FTP(self)
 
-class LocalClient:
-	'''An API client connecting to the server on the same computer'''
+class LocalClient(GenericClient):
+	"""An API client connecting to the server on the same computer.
+	Uses a `UNIX domain' socket to communicate with the server.
+	Authentication is handled through standard UNIX credentials, so no
+	extra passwords are needed."""
 	def __init__(self, socket_path=None):
+		"""Initialize the client to connect to a local FQ CNC API server."""
 		self.socket_path = socket_path or _local_socket_path
 	def _request(self, method, path, body=None):
-		req = format_request(method, path, body, {})
+		req = _format_request(method, path, body, {})
 		sock = socket.socket(socket.AF_UNIX)
 		sock.connect(self.socket_path)
 		sock.sendall(req)
 		resp = httplib.HTTPResponse(sock, method=method)
 		resp.begin()
-		return read_response(resp)
+		return _read_response(resp)
 
-class RemoteClient:
-	'''An API client connecting to a server over HTTPS'''
+class RemoteClient(GenericClient):
+	"""An API client connecting to a remote server.  Uses HTTPS over TCP
+	to communicate with the server.  A username and password are
+	required to authenticate the client."""
 	def __init__(self, domain, username, password, port=None):
+		"""Initialize the client to connect to a remote FQ CNC API server.
+
+		The `username' and `password' are the shell user name and
+		password used to log in to either the CNC or SSH.
+		"""
 		self.domain = domain
-		self.httpconn = httplib.HTTPSConnection(domain, port or _remote_port)
+		self.httpconn = HTTPSConnection(domain, port or _remote_port)
 		self.authorization = 'basic ' + base64.b64encode('%s:%s' % (username, password))
 	def _request(self, method, path, body=None):
-		body = json.dumps(body) if body is not None else None
 		headers = self.httpconn.request(method, path, body, {
 			'Authorization': self.authorization,
 			'Content-Type': 'application/json',
 			})
 		resp = self.httpconn.getresponse()
-		return read_response(resp)
+		return _read_response(resp)
--- python/fqapi/ftp.py
+++ python/fqapi/ftp.py
@@ -1,3 +1,15 @@
+"""
+Accessor classes for handling FQ VAS FTP logins.
+
+Example:
+
+ftp = client.ftp()
+account = ftp.add('newftp', 'www/ftp', 'wo')
+account.modify(name='dropbox')
+for account in ftp.list():
+	print account.name, account.directory
+"""
+
 import fqapi
 
 RO = 'ro'
@@ -5,25 +17,28 @@ RW = 'rw'
 WO = 'wo'
 
 class Account:
+	"""A single FQ VAS FTP login."""
 	def __init__(self, conn, data):
 		self.conn = conn
-		self.setup(data)
+		self._setup(data)
 
-	def setup(self, data):
+	def _setup(self, data):
 		self.name = data['name']
 		self.directory = data['directory']
 		self.rw = data['rw']
 
 	def delete(self):
+		"""Delete this account on the server."""
 		self.conn.delete('/1/ftp/%s' % self.name)
 
 	def modify(self, name=None, directory=None, rw=None):
+		"""Modify this account on the server."""
 		data = self.conn.put('/1/ftp/%s' % self.name, {
 			'name': name or self.name,
 			'directory': directory or self.directory,
 			'rw': rw or self.rw,
 			})
-		self.setup(data)
+		self._setup(data)
 		return self
 
 class FTP:
@@ -31,6 +46,10 @@ class FTP:
 		self.conn = conn
 
 	def list(self):
+		"""List all the VAS FTP accounts.
+
+		Returns a list of Account objects.
+		"""
 		data = self.conn.get('/1/ftp')
 		return [
 			Account(self.conn, item['name'], item['directory'], item['rw'])
@@ -38,6 +57,10 @@ class FTP:
 			]
 
 	def add(self, name, directory, rw):
+		"""Add an VAS FTP account.
+
+		Returns a new Account object containing the current settings.
+		"""
 		data = self.conn.post('/1/ftp', {
 			'name': name,
 			'directory': directory,
@@ -46,6 +69,7 @@ class FTP:
 		return Account(self.conn, data['name'], data['directory'], data['rw'])
 
 	def delete(self, name):
+		"""Delete the named VAS FTP account."""
 		self.conn.delete('/1/ftp/%s'%name)
 
 	def __iter__(self):
